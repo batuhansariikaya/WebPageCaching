4dbc26100bd944a9affdf078dbcb1c5ee3bb4d3a
batuhansariikaya <66845211+batuhansariikaya@users.noreply.github.com>
2023-03-16 15:52:49 +0300

    Web Page Caching

---
+++ WebPageCaching/WebPageCaching/Controllers/CacheController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.IO;
+
+namespace WebPageCaching.Controllers
+{
+    public class CacheController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public CacheController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Views/Test.cshtml");
+            byte[] fileByte=System.IO.File.ReadAllBytes(path);
+            _distributedCache.Set("dosya", fileByte);
+            return View();
+        }
+        public IActionResult ShowFile()
+        {
+            byte[] webPage = _distributedCache.Get("dosya");
+            return File(webPage,"text/html");
+        }
+    }
+}
